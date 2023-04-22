d46354da3a38d64a16b709faaf2ff7d43e3f1e9f
Puskass <cosovictarik711@gmail.com>
2023-04-22 18:10:52 +0200

    fix

--- src/data/tours.jsx
+++ src/data/tours.jsx
@@ -8,7 +8,7 @@ export const tours = [
       "Experience the breathtaking views of Mount Everest as you trek to its base camp. This 16-day trekking journey takes you through beautiful mountain landscapes and Sherpa villages.",
     condition: "K3",
     technique: "T3",
-    guides: [guides[0]],
+    guide: [guides[0]],
     price: 2500,
   },
   {
@@ -20,7 +20,7 @@ export const tours = [
       "Trek to the top of the highest mountain in Africa! This 7-day trekking adventure takes you through beautiful forests, moorlands, and alpine deserts before reaching the snow-capped summit.",
     condition: "K2",
     technique: "T2",
-    guides: [guides[1]],
+    guide: [guides[1]],
     price: 1800,
   },
   {
@@ -32,7 +32,7 @@ export const tours = [
       "Climb the highest peak in the Alps and experience stunning views of France, Italy, and Switzerland. This 8-day mountaineering expedition takes you through beautiful glaciers and rock formations before reaching the summit.",
     condition: "K4",
     technique: "T4",
-    guides: [guides[2]],
+    guide: [guides[2]],
     price: 3500,
   },
   {
@@ -44,7 +44,7 @@ export const tours = [
       "Explore the beautiful wilderness of Patagonia on foot. This 12-day trekking adventure takes you through stunning landscapes and wildlife before reaching the famous Torres del Paine national park.",
     condition: "K3",
     technique: "T2",
-    guides: [guides[3]],
+    guide: [guides[3]],
     price: 2800,
   },
   {
@@ -56,7 +56,7 @@ export const tours = [
       "Experience the thrill of mountaineering in the beautiful Alps. This 10-day expedition takes you through some of the most challenging peaks in the region, with stunning views at every step.",
     condition: "K4",
     technique: "T4",
-    guides: [guides[4]],
+    guide: [guides[4]],
     price: 4200,
   },
   {
@@ -68,7 +68,7 @@ export const tours = [
       "Explore the stunning landscapes and cultures of the Himalayas. This 14-day trekking journey takes you through beautiful forests, high passes, and remote villages before reaching the stunning Annapurna Base Camp.",
     condition: "K3",
     technique: "T3",
-    guides: [guides[5]],
+    guide: [guides[5]],
     price: 3200,
   },
 ];
