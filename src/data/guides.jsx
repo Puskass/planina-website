2f09a49014c8dbf8215db378cfc05c56db074adf
Puskass <cosovictarik711@gmail.com>
2023-04-22 18:03:44 +0200

    static data for guides and tours

---
+++ src/data/guides.jsx
@@ -0,0 +1,44 @@
+export const guides = [
+  {
+    name: "John Smith",
+    image:
+      "https://mountaintrip.com/wp-content/uploads/2018/11/Nick-Nason-250x274.jpg",
+    description:
+      "Certified mountain guide with over 10 years of experience. Specializes in guiding difficult ascents and multi-day expeditions.",
+  },
+  {
+    name: "Anna Lee",
+    image:
+      "https://images.squarespace-cdn.com/content/v1/5ca89e7cb7c92c698d84f0c2/1592333725566-3N1IVRR0OJY1AVYMM1I0/bjorg-selvag.jpg",
+    description:
+      "Experienced guide with a passion for introducing beginners to the mountains. Provides personalized instruction and guidance for all skill levels.",
+  },
+  {
+    name: "Mike Johnson",
+    image:
+      "https://mountaintrip.com/wp-content/uploads/2018/11/Nick-Nason-250x274.jpg",
+    description:
+      "Expert in backcountry skiing and snowboarding. Has extensive knowledge of avalanche safety and backcountry travel.",
+  },
+  {
+    name: "Sara Garcia",
+    image:
+      "https://images.squarespace-cdn.com/content/v1/5ca89e7cb7c92c698d84f0c2/1592333725566-3N1IVRR0OJY1AVYMM1I0/bjorg-selvag.jpg",
+    description:
+      "Specializes in guiding women's groups on mountain expeditions. Advocates for inclusivity and diversity in the outdoor industry.",
+  },
+  {
+    name: "Tom Smith",
+    image:
+      "https://mountaintrip.com/wp-content/uploads/2018/11/Nick-Nason-250x274.jpg",
+    description:
+      "Guide with a focus on sustainable and responsible outdoor practices. Works to preserve the natural environment while guiding clients on mountain adventures.",
+  },
+  {
+    name: "David Kim",
+    image:
+      "https://mountaintrip.com/wp-content/uploads/2018/11/Nick-Nason-250x274.jpg",
+    description:
+      "Expert in alpine climbing and mountaineering. Has summited some of the highest peaks in the world and guides clients on challenging ascents.",
+  },
+];
