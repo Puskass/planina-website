1cb56f3d75605cbba39175148b4a19702205f5bb
Puskass <cosovictarik711@gmail.com>
2023-04-22 18:35:53 +0200

    tour form done

---
+++ src/data/tours.jsx
@@ -0,0 +1,74 @@
+export const tours = [
+  {
+    name: "Mount Everest Base Camp",
+    image:
+      "https://media.istockphoto.com/id/1288385045/photo/snowcapped-k2-peak.jpg?b=1&s=170667a&w=0&k=20&c=D0etCDylQ3XZFqnKD-9k_EVge1SI7Cy3Sr2-jIilukU=",
+    date: "2023-10-01",
+    description:
+      "Experience the breathtaking views of Mount Everest as you trek to its base camp. This 16-day trekking journey takes you through beautiful mountain landscapes and Sherpa villages.",
+    condition: "K3",
+    technique: "T3",
+    guide: [guides[0]],
+    price: 2500,
+  },
+  {
+    name: "Kilimanjaro Trek",
+    image:
+      "https://media.istockphoto.com/id/1288385045/photo/snowcapped-k2-peak.jpg?b=1&s=170667a&w=0&k=20&c=D0etCDylQ3XZFqnKD-9k_EVge1SI7Cy3Sr2-jIilukU=",
+    date: "2024-01-15",
+    description:
+      "Trek to the top of the highest mountain in Africa! This 7-day trekking adventure takes you through beautiful forests, moorlands, and alpine deserts before reaching the snow-capped summit.",
+    condition: "K2",
+    technique: "T2",
+    guide: [guides[1]],
+    price: 1800,
+  },
+  {
+    name: "Mont Blanc Summit",
+    image:
+      "https://media.istockphoto.com/id/1288385045/photo/snowcapped-k2-peak.jpg?b=1&s=170667a&w=0&k=20&c=D0etCDylQ3XZFqnKD-9k_EVge1SI7Cy3Sr2-jIilukU=",
+    date: "2024-07-01",
+    description:
+      "Climb the highest peak in the Alps and experience stunning views of France, Italy, and Switzerland. This 8-day mountaineering expedition takes you through beautiful glaciers and rock formations before reaching the summit.",
+    condition: "K4",
+    technique: "T4",
+    guide: [guides[2]],
+    price: 3500,
+  },
+  {
+    name: "Patagonia Trek",
+    image:
+      "https://media.istockphoto.com/id/1288385045/photo/snowcapped-k2-peak.jpg?b=1&s=170667a&w=0&k=20&c=D0etCDylQ3XZFqnKD-9k_EVge1SI7Cy3Sr2-jIilukU=",
+    date: "2023-12-01",
+    description:
+      "Explore the beautiful wilderness of Patagonia on foot. This 12-day trekking adventure takes you through stunning landscapes and wildlife before reaching the famous Torres del Paine national park.",
+    condition: "K3",
+    technique: "T2",
+    guide: [guides[3]],
+    price: 2800,
+  },
+  {
+    name: "Alps Mountaineering",
+    image:
+      "https://media.istockphoto.com/id/1288385045/photo/snowcapped-k2-peak.jpg?b=1&s=170667a&w=0&k=20&c=D0etCDylQ3XZFqnKD-9k_EVge1SI7Cy3Sr2-jIilukU=",
+    date: "2024-08-15",
+    description:
+      "Experience the thrill of mountaineering in the beautiful Alps. This 10-day expedition takes you through some of the most challenging peaks in the region, with stunning views at every step.",
+    condition: "K4",
+    technique: "T4",
+    guide: [guides[4]],
+    price: 4200,
+  },
+  {
+    name: "Himalayan Trekking",
+    image:
+      "https://media.istockphoto.com/id/1288385045/photo/snowcapped-k2-peak.jpg?b=1&s=170667a&w=0&k=20&c=D0etCDylQ3XZFqnKD-9k_EVge1SI7Cy3Sr2-jIilukU=",
+    date: "2024-04-01",
+    description:
+      "Explore the stunning landscapes and cultures of the Himalayas. This 14-day trekking journey takes you through beautiful forests, high passes, and remote villages before reaching the stunning Annapurna Base Camp.",
+    condition: "K3",
+    technique: "T3",
+    guide: [guides[5]],
+    price: 3200,
+  },
+];
