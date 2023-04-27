9183ff2827f74a722ea87b8fb8d88f9b4e767b16
EncyJS <mc.ency11@gmail.com>
2023-04-28 00:16:55 +0200

    Inital Commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Home from "./pages/Home";
 import AboutUs from "./pages/AboutUs";
 import Contact from "./pages/Contact";
 import Membership from "./pages/Membership";
-import ToursList from "./pages/ToursList";
 import AddTour from "./pages/AddTour";
 import Navbar from "./shared/UIElemets/Navbar";
 
