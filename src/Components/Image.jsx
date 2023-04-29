667ba5244a85b516318c83ca56a99097e6afdbf2
EncyJS <mc.ency11@gmail.com>
2023-04-29 17:16:53 +0200

    fixed button and translated to english

--- src/Components/Image.jsx
+++ src/Components/Image.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
+import Button from "../shared/UIElemets/Button";
 
 const Image = () => {
   const history = useHistory();
@@ -11,13 +12,12 @@ const Image = () => {
   return (
     <div>
       <div className="flex justify-center items-center absolute w-[100%] h-[44rem]">
-        <button className="flex flex-col items-center justify-center gap-2.5 absolute w-[20rem] h-[5rem] bg-red-600 rounded-[1rem] bottom-[10rem]" onClick={handleFindTour}>
-          <h1 className="w-[213px] h-11 not-italic font-medium text-3xl leading-[44px] text-white font-inter">Find tour</h1>
-        </button>
+        <Button className="w-[14rem] h-[3rem]" onClick={handleFindTour}><h1>Find tour</h1>
+        </Button>
       </div>
       <img
         src="https://images.hdqwalls.com/download/colorado-mountains-4k-2560x1440.jpg"
-        alt="Slika iz Figma dizajna"
+        alt="Planina babemi"
         className="flex w-[100vw] h-[44rem]"
       />
     </div>
