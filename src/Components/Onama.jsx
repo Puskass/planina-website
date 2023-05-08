a98a12fe8016835d603e0411d836aa153bbe7f33
EncyJS <mc.ency11@gmail.com>
2023-05-08 10:22:43 +0200

    Fixed About Us section and other sizing properties

--- src/Components/Onama.jsx
+++ src/Components/Onama.jsx
@@ -2,12 +2,13 @@ import React from "react";
 
 const Onama = () => {
   return (
-    <div className="relative">
-      <div>
+    <section className="block">
+      <h1 className="font-normal text-5xl leading-tight text-black m-20">
+        About us
+      </h1>
 
-        <h1 className="font-normal text-5xl leading-tight text-black ml-20 mt-10 top-0">About us</h1>
-
-        <div className="justify-center align-center box-border w-[85rem] h-[25rem] rounded-[10px] border-[3px] border-solid border-[#F60201] p-12 m-20">
+      <div className="flex justify-center items-center">
+        <div className="max-w-[85rem] max-h-[25rem] rounded-[10px] border-[3px] border-solid border-[#F60201] p-12 m-18">
           <h1 className="font-normal text-[35px] leading-[42px] text-black">
             The society was founded by a group of enthusiasts who gained their
             mountaineering experience within their families, various other
@@ -21,7 +22,7 @@ const Onama = () => {
           </h1>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
--- src/Components/Vodici.jsx
+++ src/Components/Vodici.jsx
@@ -2,17 +2,15 @@ import React from "react";
 
 const Vodici = () => {
   return (
-    <div className="relative">
+    
+      <section >
+        <h1 className="font-normal text-5xl text-black m-20">
+          Our Guides
+        </h1>
 
-    <h1 className="absolute font-normal text-5xl leading-tight text-black left-16 mt-3rem">
-      Our Guides
-    </h1>
+        <div className="w-[85rem] h-[25rem] m-25"></div>
+      </section>
 
-    <div className="w-[85rem] h-[25rem] m-25">
-
-    </div>
- 
-  </div>
   );
 };
 
