5738e238e7110cf0d884a156923cf3b3f9898df6
haraaaaaaa <harunhebibovic05@gmail.com>
2023-05-12 01:02:50 +0200

    added location property on tour class

--- api/models/Tour.js
+++ api/models/Tour.js
@@ -5,15 +5,17 @@ const { ObjectId } = require("bson");
 module.exports = class Tour {
   constructor(
     name,
+    location,
     imgUrl,
     date,
     description,
     condition,
     technique,
     price,
-    guideId  
+    guideId
   ) {
     this.name = name;
+    this.location = location;
     this.imgUrl = imgUrl;
     this.date = date;
     this.description = description;
