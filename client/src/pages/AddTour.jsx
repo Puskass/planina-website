24b569bac5e8edf377b7b2f05100315793144229
Tarik Ćosović <97788432+Puskass@users.noreply.github.com>
2023-05-17 14:53:48 +0200

    Update AddTour.jsx

--- client/src/pages/AddTour.jsx
+++ client/src/pages/AddTour.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import Button from "../shared/UIElemets/Button";
 import { useHistory } from "react-router-dom";
