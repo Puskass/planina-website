4a624419f9deaeb25fe9d5533aaa697ba8fb52c6
Puskass <cosovictarik711@gmail.com>
2023-05-17 13:42:21 +0200

    better code, removed unnecessary hooks and styles

--- client/src/Components/Image.jsx
+++ client/src/Components/Image.jsx
@@ -1,20 +1,14 @@
 import React from "react";
-import { useHistory } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Button from "../shared/UIElemets/Button";
 
 const Image = () => {
-  const history = useHistory();
-
-  const handleFindTour = () => {
-    history.push("/tours");
-  };
-
   return (
     <div className="relative">
-      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 max-w-full h-auto">
-        <Button className="w-[14rem] h-[3rem]" onClick={handleFindTour}>
-          <h1>Find tour</h1>
-        </Button>
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        <Link to="/tours">
+          <Button className="w-56 h-12">Find tour</Button>
+        </Link>
       </div>
       <img
         src="https://images.hdqwalls.com/download/colorado-mountains-4k-2560x1440.jpg"
